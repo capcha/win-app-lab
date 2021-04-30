ceadd8f4c3f529ba5f59580006d540a1e9f425dc
capcha <arjen.robben2012@yandex.ru>
2021-04-30 12:53:01 +0700

    Сделал сборку с файла

--- win-app-lab/Info.cpp
+++ win-app-lab/Info.cpp
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define UNICODE
 #include <stdio.h>
-#include <tchar.h>
 #include <windows.h>
 
 extern "C" _declspec(dllexport) int Information(char* InfoString) 
--- win-app-lab/win-app-lab.cpp
+++ win-app-lab/win-app-lab.cpp
@@ -1,10 +1,31 @@
 ﻿// win-app-lab.cpp : Определяет точку входа для приложения.
 //
-
-#include "framework.h"
-#include "win-app-lab.h"
+// // При включении SDKDDKVer.h будет задана самая новая из доступных платформ Windows.
+// Если вы планируете сборку приложения для предыдущей версии платформы Windows, включите WinSDKVer.h и
+// задайте желаемую платформу в макросе _WIN32_WINNT, прежде чем включать SDKDDKVer.h.
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+// Файлы заголовков Windows
+#include <windows.h>
+// Файлы заголовков среды выполнения C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
 #include <stdio.h>
 #include <string>
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_WINAPPLAB_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_WINAPPLAB			107
+#define IDI_SMALL				108
+#define IDC_WINAPPLAB			109
+#define IDC_MYICON				2
 
 
 #define BUF_SIZE 32
@@ -13,104 +34,95 @@ TCHAR  currentYear[BUF_SIZE];
 
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
-LPCWSTR szTitle = L"УРВС ВАРИАНТ 18 ФАДЕЙКИН";  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+static TCHAR szWindowClass[] = _T("DesktopApp");
+static TCHAR szTitle0[] = _T("УРВС ВАРИАНТ 18 ФАДЕЙКИН");
 
 // Отправить объявления функций, включенных в этот модуль кода:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+int CALLBACK WinMain(
+   _In_ HINSTANCE hInstance,
    _In_opt_ HINSTANCE hPrevInstance,
-   _In_ LPWSTR    lpCmdLine,
-   _In_ int       nCmdShow)
+   _In_ LPSTR     lpCmdLine,
+   _In_ int       nCmdShow
+)
 {
-   UNREFERENCED_PARAMETER(hPrevInstance);
-   UNREFERENCED_PARAMETER(lpCmdLine);
-
-   // Инициализация глобальных строк
-   LoadStringW(hInstance, IDC_WINAPPLAB, szWindowClass, MAX_LOADSTRING);
-   MyRegisterClass(hInstance);
-
-   // Выполнить инициализацию приложения:
-   if (!InitInstance(hInstance, nCmdShow))
-   {
-      return FALSE;
-   }
-
-   HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINAPPLAB));
-
-   MSG msg;
-
-   // Цикл основного сообщения:
-   while (GetMessage(&msg, nullptr, 0, 0))
-   {
-      if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-      {
-         TranslateMessage(&msg);
-         DispatchMessage(&msg);
-      }
-   }
-
-   return (int)msg.wParam;
-}
-
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  ЦЕЛЬ: Регистрирует класс окна.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-   WNDCLASSEXW wcex;
+   WNDCLASSEX wcex;
 
    wcex.cbSize = sizeof(WNDCLASSEX);
-
    wcex.style = CS_HREDRAW | CS_VREDRAW;
    wcex.lpfnWndProc = WndProc;
    wcex.cbClsExtra = 0;
    wcex.cbWndExtra = 0;
    wcex.hInstance = hInstance;
-   wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPPLAB));
-   wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+   wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+   wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-   wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINAPPLAB);
+   wcex.lpszMenuName = NULL;
    wcex.lpszClassName = szWindowClass;
-   wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+   wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
-   return RegisterClassExW(&wcex);
-}
+   if (!RegisterClassEx(&wcex))
+   {
+      MessageBox(NULL,
+         _T("Call to RegisterClassEx failed!"),
+         _T("Windows Desktop Guided Tour"),
+         NULL);
 
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
-//
-//   КОММЕНТАРИИ:
-//
-//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится главное окно программы.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+      return 1;
+   }
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      150, 250, 400, 150, nullptr, nullptr, hInstance, nullptr);
+   // Store instance handle in our global variable
+   hInst = hInstance;
+
+   // The parameters to CreateWindow explained:
+   // szWindowClass: the name of the application
+   // szTitle: the text that appears in the title bar
+   // WS_OVERLAPPEDWINDOW: the type of window to create
+   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+   // 500, 100: initial size (width, length)
+   // NULL: the parent of this window
+   // NULL: this application does not have a menu bar
+   // hInstance: the first parameter from WinMain
+   // NULL: not used in this application
+   HWND hWnd = CreateWindow(
+      szWindowClass,
+      szTitle0,
+      WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
+      CW_USEDEFAULT, CW_USEDEFAULT,
+      500, 100,
+      HWND_DESKTOP,
+      NULL,
+      hInstance,
+      NULL
+   );
 
    if (!hWnd)
    {
-      return FALSE;
+      MessageBox(NULL,
+         _T("Call to CreateWindow failed!"),
+         _T("Windows Desktop Guided Tour"),
+         NULL);
+
+      return 1;
    }
 
-   ShowWindow(hWnd, nCmdShow);
+   // The parameters to ShowWindow explained:
+   // hWnd: the value returned from CreateWindow
+   // nCmdShow: the fourth parameter from WinMain
+   ShowWindow(hWnd,
+      nCmdShow);
    UpdateWindow(hWnd);
 
-   return TRUE;
+   // Main message loop:
+   MSG msg;
+   while (GetMessage(&msg, NULL, 0, 0))
+   {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+   }
+
+   return (int)msg.wParam;
 }
 
 DWORD WINAPI ThreadFunc(void*)
@@ -158,30 +170,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
    switch (message)
    {
-   case WM_COMMAND:
-   {
-      int wmId = LOWORD(wParam);
-      // Разобрать выбор в меню:
-      switch (wmId)
-      {
-      case IDM_ABOUT:
-         DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-         break;
-      case IDM_EXIT:
-         DestroyWindow(hWnd);
-         break;
-      default:
-         return DefWindowProc(hWnd, message, wParam, lParam);
-      }
-   }
-   break;
    case WM_PAINT:
    {
       PAINTSTRUCT ps;
       HDC hdc = BeginPaint(hWnd, &ps);
-      std::wstring result = L"Текущий год : " + std::wstring((LPCWSTR)currentYear);
+      std::wstring result = L"Текущий год : ";
       // Вывод результата в окно
       TextOutW(hdc, 30, 30, result.c_str(), result.size());
+      TextOut(hdc, 120 + result.size(), 30, currentYear, 4);
       EndPaint(hWnd, &ps);
    }
    break;
@@ -195,29 +191,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       WaitForSingleObject(hThread, INFINITE);
 
       CloseHandle(hThread);
-   break;
+      break;
    default:
       return DefWindowProc(hWnd, message, wParam, lParam);
    }
    return 0;
-}
-
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-   UNREFERENCED_PARAMETER(lParam);
-   switch (message)
-   {
-   case WM_INITDIALOG:
-      return (INT_PTR)TRUE;
-
-   case WM_COMMAND:
-      if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-      {
-         EndDialog(hDlg, LOWORD(wParam));
-         return (INT_PTR)TRUE;
-      }
-      break;
-   }
-   return (INT_PTR)FALSE;
 }
